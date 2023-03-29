3c0ee73655f7e4375236bb197501e695e0a61765
yuxuanjerrychen01 <yuxuanjerrychen01@163.com>
2023-03-28 19:22:31 -0500

    deleting unecessary files

--- jsx src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <h1> Hi HI ! </h1>
-}
-
-export default App;
--- pics src/components/ImageShow.js
+++
@@ -1,9 +0,0 @@
-function ImageShow( {theImage} ) {
-    return (
-        <div>
-            <img src={theImage.urls.small} alt={theImage.alt_description} />
-        </div>
-    );
-}
-
-export default ImageShow;
